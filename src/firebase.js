51ccad127fb41fbf7407286b4079388833e9cacd
Rinor Sadiku <rinor.sadiku@ornio.no>
2021-03-04 22:30:56 +0100

    Removed firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,6 @@ import 'firebase/auth';
 import 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBCpcUp3mO2FN6LV8-BdV-NcDxsCPV8scQ",
-    authDomain: "vue-app-dc2e8.firebaseapp.com",
-    projectId: "vue-app-dc2e8",
-    storageBucket: "vue-app-dc2e8.appspot.com",
-    messagingSenderId: "84790958830",
-    appId: "1:84790958830:web:d0b9025429853b943b31ff"
 };
 
 firebase.initializeApp(firebaseConfig);
